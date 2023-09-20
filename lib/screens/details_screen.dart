0a335a141a4797ab68e161f9cb56877fe505cc56
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-09-20 11:35:03 -0600

    Cambios Pt1
    Se agregó un widget que no actúa como widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,8 +5,47 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //recibir argumentos
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'Sin nombre';
+
     return const Scaffold(
-      body: Text('Details Screen'),
+        body: CustomScrollView(
+      //widget con comportamiento predefinido al hacer scroll
+      slivers: [_CustomAppBar()],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      //TODO: que no desaparezca
+      //Widget para que se ajuste al tamaño
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        //eliminar el padding
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: AssetImage('assets/no-image.jpg'),
+        ),
+      ),
     );
   }
 }
