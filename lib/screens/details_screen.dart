66baf64fba66f877ce3f1fda00f972845f7a7dd0
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-09-20 11:59:57 -0600

    Update details_screen.dart
    Se agregó un widget del poster de peli

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -12,7 +12,15 @@ class DetailsScreen extends StatelessWidget {
     return const Scaffold(
         body: CustomScrollView(
       //widget con comportamiento predefinido al hacer scroll
-      slivers: [_CustomAppBar()],
+      slivers: [
+        _CustomAppBar(),
+        SliverList(
+            delegate: SliverChildListDelegate.fixed([
+          _PosterAndTitle(),
+          _PosterAndTitle(),
+          _PosterAndTitle(),
+        ]))
+      ],
     ));
   }
 }
@@ -26,26 +34,94 @@ class _CustomAppBar extends StatelessWidget {
       backgroundColor: Colors.indigo,
       expandedHeight: 200,
       floating: false,
-      //TODO: que no desaparezca
+      pinned: true,
       //Widget para que se ajuste al tamaño
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
         //eliminar el padding
-        titlePadding: EdgeInsets.all(0),
+        titlePadding: const EdgeInsets.all(0),
         title: Container(
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           color: Colors.black12,
-          child: Text(
+          child: const Text(
             'movie.title',
             style: TextStyle(fontSize: 18),
           ),
         ),
-        background: FadeInImage(
+        background: const FadeInImage(
           placeholder: AssetImage('assets/loading.gif'),
           image: AssetImage('assets/no-image.jpg'),
         ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: const FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: AssetImage('assets/no-image.jpg'),
+            height: 250,
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.titleOriginal',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(children: [
+                Icon(
+                  Icons.star_outline,
+                  size: 20,
+                  color: Color.fromARGB(255, 207, 188, 16),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'movie.voteAverage',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ])
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
