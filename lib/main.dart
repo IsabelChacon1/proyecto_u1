dab7219caa3c3a026772a92e9e41af9c84d50d2e
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-09-26 11:51:07 -0600

    Implementando Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:proyecto_unidad_1/screens/screens.dart';
 
 void main() => runApp(const MyApp());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        //Lista de providers que se van a tener
+        ChangeNotifierProvider(
+          //avisa que hay un provider
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
